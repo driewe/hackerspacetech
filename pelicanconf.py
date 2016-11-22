8c0569c31085cad5a26036cb50daa342127f3356
David Riewe <David Riewe>
2016-11-22 10:09:24 -0600

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ AUTHOR = 'Hacker Space Tech'
 ABOUT_ME = 'We a motivated group of hardware hackers who like learning about technology and building cool things.  To learn more about the display above <a href="http://hackerspacetech.com/building-a-16x32-neopixel-display.html">Click Here</a>.  To learn more about the Hacker Stars at Hacker Space Tech <a href="/pages/hacker-stars.html">Click Here</a><br><br>'
 #ABOUT_ME = 'Electronics Tech with over 20 years of experience in the computer industry.  I enjoy building micro-controlled projects, programming and web development.  <a href="/pages/bio.html"> read my full bio here</a>
 #AVATAR = '/images/avatar2.jpg'
-AVATAR = '/images/animatedlogo.gif'
+AVATAR = '/images/animatedlogocropped.gif'
 SITENAME = 'HackerSpaceTech'
 SITEURL = 'http://www.hackerspacetech.com'
 #SITELOGO = 'images/animatedlogo.gif'
